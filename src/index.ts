80207fe6034eedaa3c79a9042ea20346780d0f16
Mike Murnane <michael.j.murnane@gmail.com>
2022-05-24 10:40:33 -0400

    load S3 to json

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,56 @@
-import XLSX from "xlsx";
+import {
+  DynamoDBClient,
+  BatchExecuteStatementCommand,
+} from "@aws-sdk/client-dynamodb";
+import { S3Client } from "@aws-sdk/client-s3";
+import { GetObjectCommand } from "@aws-sdk/client-s3";
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
+import zlib from "zlib";
 import fs from "fs";
-import path from "path";
-var pdfjsLib = require("pdfjs-dist/es5/build/pdf.js");
-import axios from "axios";
-var companies = require("../comp.json");
-var contacts = require("../annots3.json");
+import type { Readable } from "stream";
 
-const App = () => {
-  const wb = XLSX.utils.book_new();
-  var first_sheet_name = wb.SheetNames[1];
-  var worksheet = wb.Sheets[first_sheet_name];
-  let aoa = [
-    [
-      "company",
-      "location",
-      "firstName",
-      "lastName",
-      "title",
-      "email",
-      "phone",
-      "fax",
-      "mobile",
-      "address1",
-      "address2",
-      "city",
-      "county",
-      "zipCode",
-      "state",
-      "codes",
-    ],
-  ];
-  /* Find desired cell */
-  contacts.forEach((x, i) => {
-    let codeArray = x.codes.map((code) => `${code.code} ${code.name}`);
-    let codeString = codeArray.join();
-    x.contacts.map((cont) => {
-      aoa.push([
-        companies.locations[i].companyName,
-        companies.locations[i].locationName,
-        cont.firstName,
-        cont.lastName,
-        cont.title,
-        cont.email,
-        cont.phone,
-        cont.fax,
-        cont.mobile,
-        x.locationInformation.physicalAddress.address1,
-        x.locationInformation.physicalAddress.address2,
-        x.locationInformation.physicalAddress.city,
-        x.locationInformation.physicalAddress.county,
-        x.locationInformation.physicalAddress.zipCode,
-        x.locationInformation.physicalAddress.state?.name || "",
-        codeString,
-      ]);
-    });
+const s3 = new S3Client({ region: "us-east-1" });
+
+async function streamToString(stream: Readable): Promise<string> {
+  return await new Promise((resolve, reject) => {
+    const chunks: Uint8Array[] = [];
+    stream.on("data", (chunk) => chunks.push(chunk));
+    stream.on("error", reject);
+    stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf-8")));
   });
-  const ws = XLSX.utils.aoa_to_sheet(aoa);
-  XLSX.utils.book_append_sheet(wb, ws, "1");
-  XLSX.writeFile(wb, "final.xlsx");
+}
+
+const App = async () => {
+  try {
+    const zippedJson = await s3.send(
+      new GetObjectCommand({
+        Key: "AWSDynamoDB/01653391142364-cee69543/data/soqv77chfe4ajlut5my64y236e.json.gz",
+        Bucket: "mike-m",
+      })
+    );
+
+    const result = zippedJson.Body as Readable;
+    const unzip = zlib.createGunzip();
+    const writeStream = fs.createWriteStream(`./ddb.json`, "utf-8");
+    result.pipe(unzip).pipe(writeStream);
+    await new Promise((resolve) => writeStream.on("finish", resolve));
+    let data = fs.readFileSync(`./ddb.json`).toString();
+    const sortedArrayOfObjects = data
+      .replace(/\n/g, "&&")
+      .split("&&")
+      .map((x) => {
+        try {
+          console.log(x);
+          return JSON.parse(x);
+        } catch (error) {
+          return JSON.parse("{}");
+        }
+      })
+      .map((obj) => (obj.Item ? unmarshall(obj.Item) : {}));
+    fs.writeFileSync("./final.json", JSON.stringify(sortedArrayOfObjects));
+  } catch (error) {
+    console.log(error);
+  }
 };
 App();
 
